359302239e6a59aafc69aa9375292adc6af5c56d
Obimba Joshua <obimz123@gmail.com>
2025-05-01 19:53:36 +0100

    Remove unused CSS styles and update Tailwind configuration to exclude CSS files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
-tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html', 
-    './src/**/*.{js,ts,jsx,tsx}',
-    './src/styles/**/*.css'
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
